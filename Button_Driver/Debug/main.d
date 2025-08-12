5d07744604dfbbe721e32eadd4a88f5b2d730f8e
melgazza <melgazza@jaguarlandrover.com>
2025-08-12 16:53:39 +0300

    Add Button_Driver_Code Example
    Add Button_Driver_Code Example

---
+++ Button_Driver/Debug/main.d
@@ -0,0 +1,48 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ti-cgt-arm_18.12.2.LTS/include/stdint.h
+main.obj: C:/ti/ti-cgt-arm_18.12.2.LTS/include/_stdint40.h
+main.obj: C:/ti/ti-cgt-arm_18.12.2.LTS/include/sys/stdint.h
+main.obj: C:/ti/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h
+main.obj: C:/ti/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h
+main.obj: C:/ti/ti-cgt-arm_18.12.2.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ti-cgt-arm_18.12.2.LTS/include/sys/_stdint.h
+main.obj: C:/ti/ti-cgt-arm_18.12.2.LTS/include/stdbool.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/debug.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h
+main.obj: D:/Tiva_C_CCS/Button_Driver/Headers/Switch_Driver.h
+
+../main.c:
+
+C:/ti/ti-cgt-arm_18.12.2.LTS/include/stdint.h:
+
+C:/ti/ti-cgt-arm_18.12.2.LTS/include/_stdint40.h:
+
+C:/ti/ti-cgt-arm_18.12.2.LTS/include/sys/stdint.h:
+
+C:/ti/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h:
+
+C:/ti/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h:
+
+C:/ti/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h:
+
+C:/ti/ti-cgt-arm_18.12.2.LTS/include/machine/_stdint.h:
+
+C:/ti/ti-cgt-arm_18.12.2.LTS/include/sys/_stdint.h:
+
+C:/ti/ti-cgt-arm_18.12.2.LTS/include/stdbool.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/debug.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h:
+
+D:/Tiva_C_CCS/Button_Driver/Headers/Switch_Driver.h:
+
